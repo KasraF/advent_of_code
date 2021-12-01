0f0635df855bab05549677b34cff59aa8ace293a
Kasra Ferdowsifard <kferdowsifard@gmail.com>
2021-12-01 08:50:08 -0800

    starting advent of code 2021

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,3 @@
-pub type Error = Box<dyn std::error::Error>;
-
-mod day_1;
-mod day_2;
-mod day_3;
-mod day_4;
-mod day_5;
-mod utils;
-
-fn main() -> Result<(), Error> {
-    println!("Hello, advent of code 2020!");
-    day_1::solve()?;
-    day_2::solve()?;
-    day_3::solve()?;
-    day_4::solve()?;
-    day_5::solve()?;
-
-    Ok(())
+fn main() {
+    println!("Hello, world!");
 }
