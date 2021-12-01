416afb559df465a02e7fdc84523b4550bd393f3a
Kasra Ferdowsifard <kferdowsifard@gmail.com>
2021-12-01 11:28:17 -0800

    day1

---
+++ src/day1.rs
@@ -0,0 +1,29 @@
+use crate::utils::{readlines, Error};
+
+fn count_increasing<T: PartialOrd>(v: &[T]) -> usize {
+    v.iter()
+        .zip(v[1..].iter())
+        .map(|(a, b)| if a < b { 1 } else { 0 })
+        .sum()
+}
+
+pub fn main(file: Option<String>) -> Result<(String, String), Error> {
+    let path = file.unwrap_or_else(|| "resources/day1.txt".to_string());
+    let lines: Vec<i32> = readlines(path)?
+        .iter()
+        .map(|l| l.parse::<i32>().unwrap()) // TODO Is there a good clean way to handle this?
+        .collect();
+
+    let part1: usize = count_increasing(&lines);
+
+    let part2_windows: Vec<i32> = lines
+        .iter()
+        .zip(lines[1..].iter())
+        .zip(lines[2..].iter())
+        .map(|((a, b), c)| a + b + c)
+        .collect();
+
+    let part2: usize = count_increasing(&part2_windows);
+
+    Ok((format!("{}", part1), format!("{}", part2)))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use crate::utils::Error;
+
+mod day1;
+mod utils;
+
+fn main() -> Result<(), Error> {
+    let solutions = [crate::day1::main];
+    let mut args = std::env::args();
+    args.next(); // Skip the program name
+    let day = args
+        .next()
+        .expect("Please specify the day")
+        .parse::<usize>()
+        .expect("Failed to parse day number");
+    let file = args.next();
+
+    if day > solutions.len() {
+        if day <= 31 {
+            eprintln!("Day {} not yet implemented.", day);
+        } else {
+            eprintln!("What calendar are you even using?!");
+        }
+        std::process::exit(1);
+    }
+
+    let (part1, part2) = solutions[day - 1](file)?;
+    println!("Day {}: {}, {}", day, part1, part2);
+    Ok(())
 }
