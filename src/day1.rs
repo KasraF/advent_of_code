7ff8b71e9f962c390f1418cf4ca944248b88b5b1
Kasra Ferdowsifard <kferdowsifard@gmail.com>
2023-12-01 13:50:19 -0800

    finished day 1

--- src/day1.rs
+++ src/day1.rs
@@ -1,52 +1,112 @@
 use crate::Error;
-use regex::Regex;
-use smallvec::SmallVec;
 use std::{fs::File, io::BufRead, io::BufReader};
 
-fn parse_digit(d: &str) -> Result<u32, Error> {
-    match d {
-        "one" | "1" => Ok(1),
-        "two" | "2" => Ok(2),
-        "three" | "3" => Ok(3),
-        "four" | "4" => Ok(4),
-        "five" | "5" => Ok(5),
-        "six" | "6" => Ok(6),
-        "seven" | "7" => Ok(7),
-        "eight" | "8" => Ok(8),
-        "nine" | "9" => Ok(9),
-        _ => Err(format!("Not a valid digit: {d}").into()),
+#[inline]
+fn nom(s: &str, target: &str) -> bool {
+    if s.len() < target.len() {
+        return false;
     }
+
+    &s[0..target.len()] == target
 }
 
-/// Parse a given line.
-/// TODO Good excuse to learn nom here?
+fn parse_digit(slice: &str) -> Option<u32> {
+    let rs = match &slice[0..=0] {
+        "0" => 0,
+        "1" => 1,
+        "2" => 2,
+        "3" => 3,
+        "4" => 4,
+        "5" => 5,
+        "6" => 6,
+        "7" => 7,
+        "8" => 8,
+        "9" => 9,
+        "o" if nom(slice, "one") => 1,
+        "t" if nom(slice, "two") => 2,
+        "t" if nom(slice, "three") => 3,
+        "f" if nom(slice, "four") => 4,
+        "f" if nom(slice, "five") => 5,
+        "s" if nom(slice, "six") => 6,
+        "s" if nom(slice, "seven") => 7,
+        "e" if nom(slice, "eight") => 8,
+        "n" if nom(slice, "nine") => 9,
+        _ => return None,
+    };
+    Some(rs)
+}
+
 fn parse_digits(line: &str) -> u32 {
-    let digit_rx =
-        Regex::new(r"(\d)|(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)").unwrap();
-    let matches: SmallVec<[&str; 8]> = digit_rx
-        .captures_iter(line)
-        .map(|x| x.iter().next().unwrap().unwrap().as_str())
-        .collect();
-    let first = parse_digit(matches[0]).unwrap();
-    let last = parse_digit(matches[matches.len() - 1]).unwrap();
-    println!("{line}: {matches:?} -> {}", first * 10 + last);
-    first * 10 + last
+    let l = line.len();
+    let mut first = 0;
+    for i in 0..l {
+        if let Some(digit) = parse_digit(&line[i..]) {
+            first = digit;
+            break;
+        }
+    }
+
+    let mut second = 0;
+    for i in (0..l).rev() {
+        if let Some(digit) = parse_digit(&line[i..]) {
+            second = digit;
+            break;
+        }
+    }
+
+    first * 10 + second
 }
 
 pub fn main() -> Result<(), Error> {
     let rs: u32 = BufReader::new(File::open("inputs/day1.txt")?)
         .lines()
         .filter(|line| match line {
-            Ok(_) => true,
+            Ok(line) => !line.is_empty(),
             Err(e) => {
                 eprintln!("Failed to read line: {e}");
                 false
             }
         })
-        .map(|line| parse_digits(&line.unwrap()))
+        .map(|line| line.unwrap())
+        .map(|line| parse_digits(&line))
         .sum();
 
     println!("Day 1, Answer 1: {rs}");
 
     Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::parse_digits;
+
+    #[test]
+    fn example() {
+        let input = "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen";
+        let rs: u32 = input.lines().map(|l| parse_digits(l)).sum();
+        assert_eq!(rs, 281);
+    }
+
+    #[test]
+    fn test_parse_digits() {
+        assert_eq!(parse_digits("two1nine"), 29);
+        assert_eq!(parse_digits("eightwothree"), 83);
+        assert_eq!(parse_digits("abcone2threexyz"), 13);
+        assert_eq!(parse_digits("xtwone3four"), 24);
+        assert_eq!(parse_digits("4nineeightseven2"), 42);
+        assert_eq!(parse_digits("zoneight234"), 14);
+        assert_eq!(parse_digits("7pqrstsixteen"), 76);
+        assert_eq!(parse_digits("nineasd"), 99);
+
+        assert_eq!(parse_digits("1abc2"), 12);
+        assert_eq!(parse_digits("pqr3stu8vwx"), 38);
+        assert_eq!(parse_digits("a1b2c3d4e5five"), 15);
+        assert_eq!(parse_digits("treb7uchet"), 77);
+    }
 }
