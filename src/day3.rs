fa78f4663be67c9fe13c8b31fb4ae7cd3c260600
Kasra Ferdowsifard <kferdowsifard@gmail.com>
2023-12-03 10:15:11 -0800

    day 3 answer 2

--- src/day3.rs
+++ src/day3.rs
@@ -4,6 +4,28 @@ struct Schematic {
     grid: [[char; 142]; 142],
 }
 
+impl Schematic {
+    fn parse_number(&self, mut i: usize, j: usize) -> Option<u32> {
+        if !self.grid[j][i].is_ascii_digit() {
+            None
+        } else {
+            // First, see if we need to move i to the left.
+            while self.grid[j][i - 1].is_ascii_digit() {
+                i -= 1;
+            }
+
+            // Then start reading the number.
+            let mut num = self.grid[j][i].to_digit(10).unwrap();
+            i += 1;
+            while self.grid[j][i].is_ascii_digit() {
+                num = num * 10 + self.grid[j][i].to_digit(10).unwrap();
+                i += 1;
+            }
+            Some(num)
+        }
+    }
+}
+
 impl FromIterator<String> for Schematic {
     fn from_iter<T: IntoIterator<Item = String>>(iter: T) -> Self {
         let mut grid = [['.'; 142]; 142];
@@ -20,10 +42,8 @@ impl FromIterator<String> for Schematic {
     }
 }
 
-pub fn main() -> Result<(), utils::Error> {
-    let schematic: Schematic = utils::read_lines(3)?.collect();
+fn part_one(schematic: &Schematic) {
     let mut sum = 0;
-
     for (j, line) in schematic.grid[1..141].iter().enumerate() {
         let j = j + 1;
         let mut i = 1;
@@ -66,9 +86,6 @@ pub fn main() -> Result<(), utils::Error> {
                         sum += num;
                     }
                 }
-                '.' => {
-                    i += 1;
-                }
                 _ => {
                     i += 1;
                 }
@@ -77,6 +94,73 @@ pub fn main() -> Result<(), utils::Error> {
     }
 
     println!("Day 3, Answer 1: {sum}");
+}
+
+fn part_two(schematic: &Schematic) {
+    let mut sum: u128 = 0;
+
+    for (j, line) in schematic.grid[1..141].iter().enumerate() {
+        let j = j + 1;
+
+        for i in 1..(line.len() - 1) {
+            match line[i] {
+                '*' => {
+                    // Found a gear. Time to look for numbers.
+                    let right = schematic.parse_number(i + 1, j);
+                    let left = schematic.parse_number(i - 1, j);
+                    // Now above.
+                    let (top, tl, tr) = if let Some(t) = schematic.parse_number(i, j - 1) {
+                        // There is exactly one number above
+                        (Some(t), None, None)
+                    } else {
+                        // Check left and right
+                        let tl = schematic.parse_number(i - 1, j - 1);
+                        let tr = schematic.parse_number(i + 1, j - 1);
+                        (None, tl, tr)
+                    };
+
+                    // As above, so below.
+                    let (bot, bl, br) = if let Some(t) = schematic.parse_number(i, j + 1) {
+                        // There is exactly one number above
+                        (Some(t), None, None)
+                    } else {
+                        // Check left and right
+                        let tl = schematic.parse_number(i - 1, j + 1);
+                        let tr = schematic.parse_number(i + 1, j + 1);
+                        (None, tl, tr)
+                    };
+
+                    // Now, we need to check that there's *exactly* two:
+                    let nums = [left, right, top, tl, tr, bot, bl, br];
+                    let mut count = 0;
+                    let mut mul = 1;
+                    for n in nums {
+                        if let Some(n) = n {
+                            count += 1;
+                            mul *= n;
+                        }
+
+                        if count > 2 {
+                            break;
+                        }
+                    }
+
+                    if count == 2 {
+                        sum += mul as u128;
+                    }
+                }
+                _ => (),
+            }
+        }
+    }
+
+    println!("Day 3, Answer 2: {sum}");
+}
+
+pub fn main() -> Result<(), utils::Error> {
+    let schematic: Schematic = utils::read_lines(3)?.collect();
+    part_one(&schematic);
+    part_two(&schematic);
 
     Ok(())
 }
